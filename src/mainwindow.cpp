b41d02afe318ffab14a19d18dc6b456c72d36dc3
Víctor R. Ruiz <vrruiz@gmail.com>
2015-12-16 12:35:25 +0000

    Add Russian to dropdown list

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -458,8 +458,8 @@ void MainWindow::actionSettings() {
     QString defaultLanguage = settings->defaultLanguage();
     // Supported list of languages
     QStringList languageList;
-    languageList << "en-GB" << "ca-ES" << "es-ES" << "it-IT" << "pl-PL" << "pt-BR"
-                 << "pt-PT";
+    languageList << "en-GB" << "ca-ES" << "es-ES" << "it-IT" << "pl-PL"
+                 << "pt-BR" << "pt-PT" << "ru";
     SettingsDialog settingsDialog(settings, languageList, this);
     int result = settingsDialog.exec();
     if (result && settingsDialog.changed()) {
